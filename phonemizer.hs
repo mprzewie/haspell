8f031c69fa74dd3b8af71f70e9470278a0d86b64
Marcin Przewięźlikowski <m.przewie@gmail.com>
2016-12-26 18:25:27 +0100

    phonemizing nearly finished

--- phonemizer.hs
+++ phonemizer.hs
@@ -1,18 +1,36 @@
 module Phonemizer (module Phonemizer) where
 -- for converting text into phones
-import Data.Text
+import Data.Text (pack, unpack, splitOn)
 
 
 data LangRule = MkLangRule {txt::String, phones::[String]} deriving Show
 
+phonemize:: String -> IO String -> IO [String]
+phonemize lang inp=rlz>>= \r -> inp>>= \i -> 
+			if i=="" then return $ []
+			else return $ [txt ( match i r)] 
+
+	where 
+		rlz = rules lang
+
+rest:: LangRule -> String -> IO String
+rest l s= return $ reverse $ take (length s - length (txt l)) $ reverse s
+
+match:: String -> [LangRule] -> LangRule
+match _ [] = MkLangRule "" []
+match s (x:xs) = 
+	if matching x then x
+	else match s xs
+	where
+		matching a = (length s >= length (txt a))&&(foldr (&&) True (zipWith (==) s (txt a)))
 
 rules:: String -> IO [LangRule]
-rules lang= fmap (Prelude.map strToRule) rulesStringList 
+rules lang= fmap sortRules $ fmap  (map $ strToRule) rulesStringList 
 	
 	where 
 		fileCont = readFile ("lang/"++lang++"/"++lang++".hsp")
 		langName = fileCont >>= \f -> (return $ (lines' $ (splitStr "#" f)!!1)!!1)
-		rulesStringList = fileCont >>= \f -> (return  $ Prelude.tail $ lines' $ (splitStr "#" f)!!2)
+		rulesStringList = fileCont >>= \f -> (return  $ tail $ lines' $ (splitStr "#" f)!!2)
 
 strToRule::String -> LangRule
 strToRule s = MkLangRule letter phones
@@ -22,15 +40,33 @@ strToRule s = MkLangRule letter phones
 		splitRule=splitStr "->" $ despace s
 
 lines' :: String -> [String]
-lines' s = Prelude.filter (\x -> not (x=="")) $ Prelude.lines s
+lines' s = filter (\x -> not (x=="")) $ lines s
 
 splitStr :: String -> String -> [String]
 splitStr regex input = Prelude.map unpack $ splitOn (pack regex) $ pack input
 
 despace :: String -> String 
-despace s = Prelude.filter (\x -> not(x==' ')) s
+despace s = filter (\x -> not(x==' ')) s
 
 printList :: Show a => [a] => IO()
 printList []=putStr ""
 printList (x:xs) = print x >>printList xs
+
+sortRules :: [LangRule] -> [LangRule]
+sortRules [] = []
+sortRules xl = rl ++ ml ++ ll
+	where
+		piv= xl!!0
+		ll = sortRules [l | l <- xl, (length (txt l))<(length (txt piv))]
+		ml = sortAlph [m | m <- xl, (length (txt m))==(length (txt piv))]
+		rl = sortRules [r | r <- xl, (length (txt r))>(length (txt piv))]
+
+sortAlph :: [LangRule] -> [LangRule]
+sortAlph [] = []
+sortAlph xl = ll ++ ml ++ rl
+	where
+		piv= xl!!0
+		ll = sortAlph [l | l <- xl,  (txt l)<(txt piv)]
+		ml = [m | m <- xl, (txt m)==(txt piv)]
+		rl = sortAlph [r | r <- xl, (txt r)>(txt piv)]
 
