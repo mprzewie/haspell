9df7da69ee630ba7e083f6a6622370cdbb4396d9
Marcin Przewięźlikowski <m.przewie@gmail.com>
2016-12-26 01:43:36 +0100

    reorganized project and started working on phonemizer

--- phonemizer.hs
+++ phonemizer.hs
@@ -1,3 +1,24 @@
-module Phoneticizer (module Phoneticizer) where
-
+module Phonemizer (module Phonemizer) where
 -- for converting text into phones
+import Data.Text
+
+
+data LangRule = MkLangRule {txt::String, phones::[String]} deriving Show
+
+
+rules:: String -> IO [String]
+rules lang= rulesStringList
+	
+	where 
+		fileCont = readFile ("lang/"++lang++"/"++lang++".hsp")
+		langName = fileCont >>= \f -> (return $ (lines' $ (splitStr "#" f)!!1)!!1)
+		rulesStringList = fileCont >>= \f -> (return  $ Prelude.tail $ lines' $ (splitStr "#" f)!!2)
+
+strToRule::String -> LangRule
+strToRule s = MkLangRule "korwo" ["mać"]
+
+lines' :: String -> [String]
+lines' s = Prelude.filter (\x -> not (x=="")) $ Prelude.lines s
+
+splitStr :: String -> String -> [String]
+splitStr regex input = Prelude.map unpack $ splitOn (pack regex) $ pack input
