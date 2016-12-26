41021eb932abcd3c1e5bf7832c0d0330b198c701
Marcin Przewięźlikowski <m.przewie@gmail.com>
2016-12-26 12:22:43 +0100

    getting rules based on file is basically done (but not finished)

--- phonemizer.hs
+++ phonemizer.hs
@@ -6,8 +6,8 @@ import Data.Text
 data LangRule = MkLangRule {txt::String, phones::[String]} deriving Show
 
 
-rules:: String -> IO [String]
-rules lang= rulesStringList
+rules:: String -> IO [LangRule]
+rules lang= fmap (Prelude.map strToRule) rulesStringList 
 	
 	where 
 		fileCont = readFile ("lang/"++lang++"/"++lang++".hsp")
@@ -15,10 +15,22 @@ rules lang= rulesStringList
 		rulesStringList = fileCont >>= \f -> (return  $ Prelude.tail $ lines' $ (splitStr "#" f)!!2)
 
 strToRule::String -> LangRule
-strToRule s = MkLangRule "korwo" ["mać"]
+strToRule s = MkLangRule letter phones
+	where
+		letter = splitRule!!0
+		phones = splitStr "," $ splitRule!!1
+		splitRule=splitStr "->" $ despace s
 
 lines' :: String -> [String]
 lines' s = Prelude.filter (\x -> not (x=="")) $ Prelude.lines s
 
 splitStr :: String -> String -> [String]
 splitStr regex input = Prelude.map unpack $ splitOn (pack regex) $ pack input
+
+despace :: String -> String 
+despace s = Prelude.filter (\x -> not(x==' ')) s
+
+printList :: Show a => [a] => IO()
+printList []=putStr ""
+printList (x:xs) = print x >>printList xs
+
