6f9fb3e6c9523944836a546c2a6363e459383568
Lukasz Nawojowski <luknwpl@gmail.com>
2017-01-07 19:10:50 +0100

    Lot of imports and new dependencies Rewriting Soundgluer in progress Soundgluer loads waves into phonem-data map

--- src/Soundgluer.hs
+++ src/Soundgluer.hs
@@ -1,31 +1,80 @@
+{-# LANGUAGE FlexibleContexts #-}
+
 module Soundgluer where
 
+import System.Exit
+import System.Directory
+
+import Control.Monad
+
+import qualified Data.Map as Map
+import qualified Data.Text as Text
+import Data.Array.IO
+import Data.Array.Unboxed
+import Data.Int
+
+import Data.Audio
 import Codec.Wav
 
-glueSpeech :: String -> [String] -> IO ()
-glueSpeech fileName phones
+
+wavFileError :: String
+wavFileError = "wav file error: "
+
+waveExtension :: FilePath
+waveExtension = ".wav"
+
+directorySeparator :: FilePath
+directorySeparator = "/"
+
+languagesPath :: FilePath
+languagesPath = "lang" ++ directorySeparator
+
+
+
+glueSpeech :: String -> [String] -> String-> IO ()
+glueSpeech language phones fileName
         | null phones = return ()
         | otherwise   = do
-            waves <- mapM phoneToWAVE phones
-            putWAVEFile (fileName++".wav") $ foldr1 concatWAVE waves
-    
-phoneToWAVE :: String -> IO WAVE
-phoneToWAVE p =
-    getWAVEFile $ "lang/pol/" ++ p ++ ".wav"
+            phoneAudioMap <- loadLanguageAudio language :: IO (Map.Map String (Audio Int32))
+            print phoneAudioMap
 
-concatWAVE :: WAVE -> WAVE -> WAVE
-concatWAVE w1 w2 = WAVE
-                   { waveHeader = WAVEHeader
-                                  { waveNumChannels = waveNumChannels . waveHeader $ w1
-                                  , waveFrameRate = waveFrameRate . waveHeader $ w1
-                                  , waveBitsPerSample = waveBitsPerSample . waveHeader $ w1
-                                  , waveFrames = Just (getFrames w1 + getFrames w2)
-                                  }
-                   , waveSamples = waveSamples w1 ++ waveSamples w2
-                   }
-    
-getFrames :: WAVE -> Int
-getFrames w =
-        case waveFrames . waveHeader $ w of
-            Nothing -> 0 -- Nothing means the actual value is unknown, so setting 0 for convenience.
-            Just x -> x
+
+loadLanguageAudio :: ( MArray IOUArray a IO
+                     , IArray UArray a
+                     , Audible a
+                     , AudibleInWav a)
+                     => String -> IO (Map.Map String (Audio a))
+loadLanguageAudio language = do
+    dirContents <- listDirectory languageDirectory
+    phoneAudioList <- zip <$> (return . map phoneName) (filter isWave dirContents)
+                          <*> forM (filter isWave dirContents) (getAudioData languageDirectory)
+    return $ Map.fromList phoneAudioList
+  where
+    languageDirectory = getLanguageDirectory language
+
+isWave :: FilePath -> Bool
+isWave fileName = waveExtension == ( Text.unpack
+                                   . Text.takeEnd (length waveExtension)
+                                   . Text.pack
+                                   $ fileName)
+
+phoneName :: FilePath -> String
+phoneName fileName = Text.unpack
+                          . Text.dropEnd (length waveExtension)
+                          . Text.pack
+                          $ fileName
+
+getLanguageDirectory :: String -> FilePath
+getLanguageDirectory language = languagesPath ++ language ++ directorySeparator
+
+getAudioData :: ( MArray IOUArray a IO
+                , IArray UArray a
+                , Audible a
+                , AudibleInWav a)
+                => FilePath -> FilePath -> IO (Audio a)
+getAudioData languageDirectory fileName = do
+    eitherWave <- importFile $ languageDirectory ++ fileName
+    case eitherWave of
+      (Left s) -> putStrLn (wavFileError ++ s) >> exitFailure
+      (Right wave) -> return wave
+
