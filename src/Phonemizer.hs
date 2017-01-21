1411edaf88015110b8582c2d1ae23a404751475b
Marcin Przewięźlikowski <m.przewie@gmail.com>
2017-01-22 00:51:58 +0100

    added comments to Interpreter and Phonemizer

--- src/Phonemizer.hs
+++ src/Phonemizer.hs
@@ -3,7 +3,7 @@ module Phonemizer (module Phonemizer) where
 import Data.Char (toLower)
 import HspInterpreter (Phone,langRules, LangRule(..), AliasRule(..), aliasRules)
 
-
+--given the ID of language and a String returns a list of "words" - lists of phones
 phonemize :: String -> String -> IO [[Phone]]
 phonemize lang inp = do
         let wrds = filter (/="") $ words (map toLower inp)
@@ -11,7 +11,6 @@ phonemize lang inp = do
         rlz <- langRules lang
         let phnmzd = map (phonemize' rlz als) wrds
         return $ phnmzd
-        --return $ foldr (\acc wrd -> acc>>= \a -> fmap (a++ wrd])) (return []) phnmzd
         where 
             phonemize':: [LangRule] -> [AliasRule] -> String-> [Phone]
             phonemize' r a i=
@@ -25,6 +24,7 @@ phonemize lang inp = do
                     let res = (phones $ matchLangRule i r) ++ rst
                      in considerAliases a $ (filter (/="") res)
 
+--given a String and a list of LangRules returns the LangRule whose token is the same as the beginning of the String
 matchLangRule:: String -> [LangRule] -> LangRule
 matchLangRule s [] = MkLangRule [(head s)] ["-"]
 matchLangRule s (x:xs) = 
@@ -34,6 +34,7 @@ matchLangRule s (x:xs) =
         matching a = (length s >= length (token a))
                      && (foldr (&&) True (zipWith (==) s (token a)))
 
+--modifies the list of phones based on AliasRules
 considerAliases :: [AliasRule] -> [Phone]-> [Phone]
 considerAliases _ []=[]
 considerAliases rules phones= maybe didntmatch didmatch $ matchAliasRule phones rules
@@ -43,8 +44,8 @@ considerAliases rules phones= maybe didntmatch didmatch $ matchAliasRule phones 
             --didmatch =(\rule -> (output rule)++(considerAliases rules $ rest rule phones))
             rest aliasrule s = drop (length (regex aliasrule)) s
 
-
-matchAliasRule :: [String] -> [AliasRule] -> Maybe AliasRule
+--given a word (a list of phones) and a list of AliasRules returns the AliasRule whose regex is the same as the beginning of the word
+matchAliasRule :: [Phone] -> [AliasRule] -> Maybe AliasRule
 matchAliasRule _ [] = Nothing
 matchAliasRule s (x:xs) = 
     if matching x then Just x
