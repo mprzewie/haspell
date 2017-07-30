577df80116c5a7f1553f9ae207d5e04eaa28e531
Marcin Przewięźlikowski <m.przewie@gmail.com>
2017-07-30 19:15:00 +0200

    added documentation to the Speaker module

--- src/Speaker.hs
+++ src/Speaker.hs
@@ -5,26 +5,32 @@ import Phonemizer (phonemize)
 import Soundgluer (glueSpeech, waveExtension)
 import System.Process(callProcess)
 
-speak :: String -> [[Phone]] -> IO()
+-- | Plays the phonems in the specified language
+speak 	:: String 		-- ^ Name of the language. It should match the name of the language folder in the langsDirectory.
+		-> [[Phone]] 	-- ^ List of lists of Phonems to speak
+		-> IO()			
 speak lang (word:rest) =  do
 	speakWord lang word
 	playFile (emptyWave)
 	speak lang rest
 speak _ [] = do return ()
 
-speakWord :: String -> [Phone] -> IO()
+-- | Creates a .tmp file with given word (list of phonems) and plays it in given language
+speakWord 	:: String 		-- ^ Name of the language. It should match the name of the language folder in the langsDirectory.
+			-> [Phone] 		-- ^ List of phonems (word) to speak
+			-> IO()
 speakWord lang word = do
 	glueSpeech lang [word] tmpFileName
 	playFile tmpFileName
 
+-- | Path to the "silent" waveFile
 emptyWave :: FilePath
 emptyWave = "lang/std/-"
 
+-- | Name of the .tmp file which w
 tmpFileName :: FilePath
 tmpFileName = ".tmp"
 
+-- | Plays the given wave file
 playFile :: String -> IO()
 playFile file = callProcess "play" [file ++ waveExtension]
-
-rmFile :: String -> IO()
-rmFile file = callProcess "rm" [file]
